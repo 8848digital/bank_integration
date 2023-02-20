efe08c4a8c6e5f580da0158c1901eda9d77efb3a
Sankara Subramanian <sankarsubramaniankvs@gmail.com>
2023-02-20 12:18:49 +0530

    feat: payment api

--- bank_integration/API/payments.py
+++ bank_integration/API/payments.py
@@ -10,10 +10,35 @@ def fund_confirmation(**kwargs):
         payload = {
                 "identification": kwargs.get('identification'),
                 }
+        header_data = {}
         headers = set_headers(header_data)
         headers['consentid'] = settings.get_password('consent_id')
         headers['customerid'] = settings.get_password('user_id')
-        url = get_url(header_data,settings) + 'fund-confirmation'
+        # url = get_url(header_data,settings) + 'fund-confirmation'
+        response = requests.request("POST", url, headers=headers, data=payload)
+        return response.json()
+    except Exception as e:
+        frappe.logger('bank').exception(e)
+        return {'error':e}
+
+
+@frappe.whitelist()
+def process_payment(**kwargs):
+    try:
+        bp = frappe.get_doc('Bank Payment',kwargs.get('doc'))
+        settings = frappe.get_doc('Bank Integration',{'bank_account':bp.company_bank_account})
+        headers = set_headers(settings)
+        url = settings.server_url + "/bank.API.yes_bank.payments.make_payment"
+        pe_list = []
+        for pe in bp.bank_payment_item:
+            doc = frappe.get_dict('Payment Entry',pe.name)
+            doc['account_doc'] = frappe.get_dict("Bank Account",doc.party_bank_account)
+            pe_list.append(doc)
+        payload = {
+            "payment_details": bp.as_dict(),
+            "pe_list": pe_list,
+            "company_account": bp.company_bank_account
+        }
         response = requests.request("POST", url, headers=headers, data=payload)
         return response.json()
     except Exception as e:
--- bank_integration/bank_integration/doctype/bank_payment/bank_payment.py
+++ bank_integration/bank_integration/doctype/bank_payment/bank_payment.py
@@ -1,8 +1,20 @@
 # Copyright (c) 2022, 8848 Digital LLP and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class BankPayment(Document):
-	pass
+	def fetch_payment_entries(self):
+		entries = frappe.get_list('Payment Entry',
+								filters={'mode_of_payment':self.payment_type,'bank_account':self.company_bank_account},
+								fields=['name','party_type','party','paid_amount'])
+		for pe in entries:
+			self.append('bank_payment_item',{
+				'payment_entry': pe.name,
+				'party_type': pe.party_type,
+				'party':pe.party,
+				'amount': pe.paid_amount
+			})
+		self.save()
